727eeb54ed4ae69b06e39e08ff561f1f175fdeb4
mcxiaoke <mcxiaoke@live.com>
2019-11-10 21:03:36 +0800

    Updates

---
+++ precache-manifest.605ef35952fcbd5b1a63fdead9c353b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36030a5944c553f701a5df833aa24c63",
+    "url": "/mqtt-monitor/index.html"
+  },
+  {
+    "revision": "e3f982c5ebd597d3657a",
+    "url": "/mqtt-monitor/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d84aead767156d0a5d10",
+    "url": "/mqtt-monitor/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e3f982c5ebd597d3657a",
+    "url": "/mqtt-monitor/static/js/2.ff1a1b1e.chunk.js"
+  },
+  {
+    "revision": "d84aead767156d0a5d10",
+    "url": "/mqtt-monitor/static/js/main.42bbc311.chunk.js"
+  },
+  {
+    "revision": "793507ad344f0f10603b",
+    "url": "/mqtt-monitor/static/js/runtime-main.5e07e2e8.js"
+  }
+]);
