13255557dd81028c420f6e20975a38beebd0f76b
mcxiaoke <mcxiaoke@live.com>
2021-03-21 18:43:13 +0800

    add docs for website

---
+++ docs/precache-manifest.77724a8e4f1aaf6ade30ce996aceb5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866deeb5580c5efd6ddd462f924c1ba9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b89af9add8d4d13dc93",
+    "url": "./static/css/2.1a1504c9.chunk.css"
+  },
+  {
+    "revision": "ef457ac82af93fe0042a",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9b89af9add8d4d13dc93",
+    "url": "./static/js/2.eef3e905.chunk.js"
+  },
+  {
+    "revision": "ef457ac82af93fe0042a",
+    "url": "./static/js/main.d9e65e74.chunk.js"
+  },
+  {
+    "revision": "e6fddde8e8d90e4b80fd",
+    "url": "./static/js/runtime-main.25df1f47.js"
+  }
+]);
